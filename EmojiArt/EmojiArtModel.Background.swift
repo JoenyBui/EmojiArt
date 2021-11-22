06d38b36aa42c5f95856597c2567a876bfdd94b2
Joeny Bui <joeny.bui@alertmedia.com>
2021-11-22 17:11:47 -0600

    Add EmojiArt file.

---
+++ EmojiArt/EmojiArtModel.Background.swift
@@ -0,0 +1,31 @@
+//
+//  EmojiArtModelBackground.swift
+//  EmojiArt
+//
+//  Created by Joeny Bui on 11/21/21.
+//
+
+import Foundation
+
+extension EmojiArtModel {
+    
+    enum Background: Equatable {
+            case blank
+            case url(URL)
+            case imageData(Data)
+            
+            var url: URL? {
+                switch self {
+                case .url(let url): return url
+                default: return nil
+                }
+            }
+            
+            var imageData: Data? {
+                switch self {
+                case .imageData(let data): return data
+                default: return nil
+                }
+            }
+        }
+}
